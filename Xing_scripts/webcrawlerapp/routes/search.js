3a2ceb9c84e1edb7d935388aef8c80d6d966ad70
Xing Fu <Xing@Xings-MacBook-Pro.local>
2016-07-05 16:32:09 -0400

    modify xing scripts

---
+++ Xing_scripts/webcrawlerapp/routes/search.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var elasticsearch = require('elasticsearch');
+var router = express.Router();
+
+var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+});
+
+var index = "scrapy";
+var type = "xiaomiapp";
+
+router.get('/', function(req, res, next) {
+  console.log(req.query.name)
+  client.search({
+    index: 'scrapy',
+	  type: 'xiaomiapp',
+	  body: {
+	    query: {
+	      match: {
+	        _all: req.query.name
+	      }
+	    }
+    }
+  })
+  .then(function(results) {
+    console.log(results);
+    res.render('apps', {applist: results.hits.hits});
+  })
+  .catch(function(err) {
+    console.log(err);
+    res.send(err);
+  })
+});
+
+module.exports = router;
